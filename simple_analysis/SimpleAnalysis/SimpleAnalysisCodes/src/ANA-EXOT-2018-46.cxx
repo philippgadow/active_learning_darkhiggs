10edfc1480d710b9db07c3c8ab3abdbfaef27ebc
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2021-06-14 09:56:01 +0200

    clean up cutflows

--- simple_analysis/SimpleAnalysis/SimpleAnalysisCodes/src/ANA-EXOT-2018-46.cxx
+++ simple_analysis/SimpleAnalysis/SimpleAnalysisCodes/src/ANA-EXOT-2018-46.cxx
@@ -13,6 +13,60 @@ DefineAnalysis(METHbb2018)
     addRegions({"MET150200_3b", "MET200350_3b", "MET350500_3b"});
     addRegions({"MET500_3b"});
 
+    // Cutflow histograms
+    // reco level cutflow merged (22 cuts)
+    // - Initial
+    // - PassGRL
+    // - passLArTile
+    // - Trigger
+    // - HasVtx
+    // - BadJet
+    // - CosmicMuon
+    // - BadMuon
+    // - PFlow Electron veto
+    // - IsMETTrigPassed
+    // - 0 baseline electrons
+    // - 0 baseline muons
+    // - Tau Veto
+    // - Extended Tau Veto
+    // - MetTST>500
+    // - >=1 fat-jets
+    // - >= 2 b-tagged track jets
+    // - (mJ > 40 || mJ_corr > 40)
+    // - (mJ> 50 && mJ < 270)
+    // - N_associated_trkJets>=2
+    // - (TrackJet_1passOR = true && TrackJet_2passOR = true)
+    // - |DeltaPhiMin3|>20deg
+    addHistogram("Cutflow_merged", 22, -0.5, 21.5);
+    // reco level cutflow resolved (26 cuts)
+    // - Initial
+    // - PassGRL
+    // - passLArTile
+    // - Trigger
+    // - HasVtx
+    // - BadJet
+    // - CosmicMuon
+    // - BadMuon
+    // - PFlow Electron veto
+    // - IsMETTrigPassed
+    // - 0 baseline electrons
+    // - 0 baseline muons
+    // - Tau Veto
+    // - Extended Tau Veto
+    // - MetTST>150
+    // - MetTST<=500
+    // - >=2 jets
+    // - >=2 b-tags
+    // - (mjj > 40 || mjj_corr > 40)
+    // - (mjj > 50 && mjj < 280)
+    // - |DeltaPhiMin3|>20deg
+    // - METSig>12
+    // - (pt_jj > 100000 || pt_jj_corr > 100000)
+    // - mT_METclosestBJet > 170000
+    // - mT_METfurthestBJet > 200000
+    // - <=4 jets
+    addHistogram("Cutflow_resolved", 26, -0.5, 25.5);
+
     // Inclusive histograms
     addHistogram("MET", 40, 0., 2000.);
     addHistogram("mHiggs_resolved", 22, 50., 280.);
@@ -131,51 +185,212 @@ void METHbb2018::ProcessEvent(AnalysisEvent *event) {
   bool passResolved = false;
 
   // common cuts in merged and resolved event selections
-  bool evtsel_met150 = (met > 150.);
-  bool evtsel_leptonVeto = (baselineLeptons.size() == 0);
+  bool evtsel_electronVeto = (baselineElectrons.size() == 0);
+  bool evtsel_muonVeto = (baselineMuons.size() == 0);
   bool evtsel_tauVeto = (baselineTaus.size() == 0);
   bool evtsel_extendedTauVeto = true; // not implemented on truth level
   bool evtsel_minDPhi20 = (dphiMin3 > 20. * M_PI / 180.);
 
   // cuts in merged selection
   bool evtsel_met500 = (met > 500.);
+  bool evtsel_nfatjets = (fatJets.size() >= 1);
+  bool evtsel_nbtrackjets = (btrackjets.size() >= 2);
+  bool evtsel_mass40_merged = (mHiggs > 40.);
   bool evtsel_massRange_merged = (mHiggs > 50. && mHiggs < 270.);
+  bool evtsel_nbtrackjets_associated = (nBJetsMerged_inside >= 2);
 
   // cuts in resolved selection
+  bool evtsel_met150 = (met > 150.);
   bool evtsel_metleq500 = (met <= 500.);
   bool evtsel_njets = (centralJets.size() >=2);
   bool evtsel_nbjets = (nBjets >=2);
-  bool evtsel_ptHiggs = ((met <= 350. && ptHiggs > 100) || (met > 350. && ptHiggs > 300));
+  bool evtsel_mass40_resolved = (mHiggs > 40.);
+  bool evtsel_massRange_resolved = (mHiggs > 50. && mHiggs < 280.);
+  bool evtsel_metSig = (metSignificance > 12.);
+
   bool evtsel_mt_mindR = (mt_min > 170.);
   bool evtsel_mt_maxdR = (mt_max > 200.);
-  bool evtsel_metSig = (metSignificance > 12.);
   bool evtsel_njets_max = ((nBjets == 2 && centralJets.size() <= 4) || (nBjets >=3 && centralJets.size() <= 5));
-  bool evtsel_massRange_resolved = (mHiggs > 50. && mHiggs < 280.);
+  bool evtsel_ptHiggs = ((met <= 350. && ptHiggs > 100) || (met > 350. && ptHiggs > 300));
 
+  // fill cutflows
+  bool passCutFlow_Preselection = true;
+  bool passCutFlow_Merged = true;
+  bool passCutFlow_Resolved = true;
+
+
+
+  // preselection
+  if (true) {
+    // reco level cuts which are not implemented at truth level
+    fill("Cutflow_merged", 0.1); // Initial
+    fill("Cutflow_merged", 1);   // PassGRL
+    fill("Cutflow_merged", 2);   // passLArTile
+    fill("Cutflow_merged", 3);   // Trigger
+    fill("Cutflow_merged", 4);   // HasVtx
+    fill("Cutflow_merged", 5);   // BadJet
+    fill("Cutflow_merged", 6);   // CosmicMuon
+    fill("Cutflow_merged", 7);   // BadMuon
+    fill("Cutflow_merged", 8);   // PFlow Electron veto
+    fill("Cutflow_merged", 9);   // IsMETTrigPassed
+
+    fill("Cutflow_resolved", 0.1); // Initial
+    fill("Cutflow_resolved", 1);   // PassGRL
+    fill("Cutflow_resolved", 2);   // passLArTile
+    fill("Cutflow_resolved", 3);   // Trigger
+    fill("Cutflow_resolved", 4);   // HasVtx
+    fill("Cutflow_resolved", 5);   // BadJet
+    fill("Cutflow_resolved", 6);   // CosmicMuon
+    fill("Cutflow_resolved", 7);   // BadMuon
+    fill("Cutflow_resolved", 8);   // PFlow Electron veto
+    fill("Cutflow_resolved", 9);   // IsMETTrigPassed
+  } else{
+    passCutFlow_Preselection = false;
+  }
+  if (passCutFlow_Preselection && evtsel_electronVeto) {
+    fill("Cutflow_merged", 10);   // 0 baseline electrons
+    fill("Cutflow_resolved", 10); // 0 baseline electrons
+  } else{
+    passCutFlow_Preselection = false;
+  }
+  if (passCutFlow_Preselection && evtsel_muonVeto) {
+    fill("Cutflow_merged", 11);   // 0 baseline muons
+    fill("Cutflow_resolved", 11); // 0 baseline muons
+  } else{
+    passCutFlow_Preselection = false;
+  }
+  if (passCutFlow_Preselection && evtsel_tauVeto) {
+    fill("Cutflow_merged", 12);   // Tau Veto
+    fill("Cutflow_resolved", 12); // Tau Veto
+  } else{
+    passCutFlow_Preselection = false;
+  }
+  if (passCutFlow_Preselection && evtsel_extendedTauVeto) {
+    fill("Cutflow_merged", 13);   // Extended Tau Veto
+    fill("Cutflow_resolved", 13); // Extended Tau Veto
+  } else{
+    passCutFlow_Preselection = false;
+  }
+
+  // merged cutflow
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_met500) {
+    fill("Cutflow_merged", 14); // MetTST>500
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_nfatjets) {
+    fill("Cutflow_merged", 15); // >=1 fat-jets
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_nbtrackjets) {
+    fill("Cutflow_merged", 16); // >= 2 b-tagged track jets
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_mass40_merged) {
+    fill("Cutflow_merged", 17); // (mJ > 40 || mJ_corr > 40)
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_massRange_merged) {
+    fill("Cutflow_merged", 18); // (mJ> 50 && mJ < 270)
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_nbtrackjets_associated) {
+    fill("Cutflow_merged", 19); // N_associated_trkJets>=2
+    fill("Cutflow_merged", 20); // (TrackJet_1passOR = true && TrackJet_2passOR = true)
+  } else{
+    passCutFlow_Merged = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Merged && evtsel_minDPhi20) {
+    fill("Cutflow_merged", 21); // |DeltaPhiMin3|>20deg
+  } else{
+    passCutFlow_Merged = false;
+  }
+
+  // resolved cutflow
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_met150) {
+    fill("Cutflow_resolved", 14); // MetTST>150
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_metleq500) {
+    fill("Cutflow_resolved", 15); // MetTST<=500
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_njets) {
+    fill("Cutflow_resolved", 16); // >=2 jets
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_nbjets) {
+    fill("Cutflow_resolved", 17); // >=2 b-tags
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_mass40_resolved) {
+    fill("Cutflow_resolved", 18); // (mjj > 40 || mjj_corr > 40)
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_massRange_resolved) {
+    fill("Cutflow_resolved", 19); // (mjj > 50 && mjj < 280)
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_minDPhi20) {
+    fill("Cutflow_resolved", 20); // |DeltaPhiMin3|>20deg
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_metSig) {
+    fill("Cutflow_resolved", 21); // METSig>12
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_ptHiggs) {
+    fill("Cutflow_resolved", 22); // (pt_jj > 100000 || pt_jj_corr > 100000)
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_mt_mindR) {
+    fill("Cutflow_resolved", 23); // mT_METclosestBJet > 170000
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_mt_maxdR) {
+    fill("Cutflow_resolved", 24); // mT_METfurthestBJet > 200000
+  } else{
+    passCutFlow_Resolved = false;
+  }
+  if (passCutFlow_Preselection && passCutFlow_Resolved && evtsel_njets_max) {
+    fill("Cutflow_resolved", 25); // <=4 jets
+  } else{
+    passCutFlow_Resolved = false;
+  }
+
+ 
   // Preselection
-  if (!evtsel_met150) return;
-  if (!evtsel_leptonVeto) return;
-  if (!evtsel_tauVeto) return;
-  if (!evtsel_extendedTauVeto) return;
-  if (!evtsel_minDPhi20) return;
+  if (!passCutFlow_Preselection) return;
 
   // Merged selection
-  passMerged = evtsel_met500 && evtsel_massRange_merged;
+  passMerged = passCutFlow_Preselection && passCutFlow_Merged;
 
   // Resolved selection
-  passResolved = evtsel_metleq500 && evtsel_njets && evtsel_nbjets && evtsel_ptHiggs && \
-                 evtsel_mt_mindR && evtsel_mt_maxdR && evtsel_metSig && evtsel_njets_max && \
-                 evtsel_massRange_resolved;
+  passResolved = passCutFlow_Preselection && passCutFlow_Resolved;
 
   // Fill histograms
-  if (passMerged || passResolved) {
+  if (passCutFlow_Preselection) {
     fill("MET", met);
   }
 
   if (passMerged) {
     fill("N_Bjets_merged_inside", nBJetsMerged_inside);
     fill("N_Bjets_merged_outside", nBJetsMerged_outside);
-    if (nBJetsMerged_inside == 2)
+    if (nBJetsMerged_inside == 2 && nBJetsMerged_outside == 0)
       fill("mHiggs_merged", mHiggs);
   }
 
